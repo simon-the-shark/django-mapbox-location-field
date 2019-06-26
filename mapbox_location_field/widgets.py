c78d2ca4e1d824377852f2572806af402606d255
simon <simon_rekin@o2.pl>
2019-06-26 21:43:56 +0200

    added MapInput templates

--- mapbox_location_field/widgets.py
+++ mapbox_location_field/widgets.py
@@ -3,18 +3,17 @@ from django.utils.html import mark_safe
 
 
 class MapInput(TextInput):
+    template_name = "mapbox_location_field\map_input.html"
+
     class Media:
         js = ("js\map_input.js",)
         css = {
             "all": ("css\map_input.css",)
         }
 
-    def render(self, name, value, attrs=None, renderer=None):
+    def get_context(self, name, value, attrs):
         attrs.update({
             "class": "js-mapbox-input-location-field",
         })
 
-        rendered_html = super().render(name, value, attrs, renderer)
-        rendered_html += "<div id='secret-id-map-mapbox-location-field' class='location-field-map'></div><div id='secret-id-geocoder-mapbox-location-field' class='location-field-geocoder'></div>"
-
-        return mark_safe(rendered_html)
+        return super().get_context(name, value, attrs)
