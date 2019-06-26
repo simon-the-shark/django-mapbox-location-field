a1544a46425808b5d27e0b087cc914c9f61d3565
simon <simon_rekin@o2.pl>
2019-06-26 16:11:58 +0200

    added include jquery tag

--- mapbox_location_field/templatetags/mapbox_location_field_tags.py
+++ mapbox_location_field/templatetags/mapbox_location_field_tags.py
@@ -8,3 +8,9 @@ register = template.Library()
 def location_field_includes():
     html = "<meta name='viewport' content='initial-scale=1,maximum-scale=1,user-scalable=no'/><script src='https://api.tiles.mapbox.com/mapbox-gl-js/v1.0.0/mapbox-gl.js'></script><link href='https://api.tiles.mapbox.com/mapbox-gl-js/v1.0.0/mapbox-gl.css' rel='stylesheet'/><script src='https://api.mapbox.com/mapbox-gl-js/plugins/mapbox-gl-geocoder/v4.4.0/mapbox-gl-geocoder.min.js'></script><link rel='stylesheet' href='https://api.mapbox.com/mapbox-gl-js/plugins/mapbox-gl-geocoder/v4.4.0/mapbox-gl-geocoder.css' type='text/css'/>"
     return mark_safe(html)
+
+
+@register.simple_tag
+def include_jquery():
+    return mark_safe(
+        "<script src='https://code.jquery.com/jquery-3.2.1.slim.min.js' integrity='sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN' crossorigin='anonymous'></script>")
