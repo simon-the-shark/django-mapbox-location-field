fd78470edd77f9be33a42de0e8fef2f459af61c2
simon <simon_rekin@o2.pl>
2019-06-26 21:27:59 +0200

    fixed MapInput rendering

--- mapbox_location_field/widgets.py
+++ mapbox_location_field/widgets.py
@@ -1,8 +1,8 @@
-from django.forms.widgets import HiddenInput
+from django.forms.widgets import TextInput
 from django.utils.html import mark_safe
 
 
-class MapInput(HiddenInput):
+class MapInput(TextInput):
     class Media:
         js = ("js\map_input.js",)
         css = {
@@ -10,7 +10,11 @@ class MapInput(HiddenInput):
         }
 
     def render(self, name, value, attrs=None, renderer=None):
-        attrs.update({"class": "js-mapbox-input-location-field"})
+        attrs.update({
+            "class": "js-mapbox-input-location-field",
+        })
+
         rendered_html = super().render(name, value, attrs, renderer)
         rendered_html += "<div id='secret-id-map-mapbox-location-field' class='location-field-map'></div><div id='secret-id-geocoder-mapbox-location-field' class='location-field-geocoder'></div>"
+
         return mark_safe(rendered_html)
