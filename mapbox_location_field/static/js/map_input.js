716224933ee3b7413880f741b00938c18206e207
simon <simon_rekin@o2.pl>
2019-06-27 10:47:42 +0200

    added reverse geocoding

--- mapbox_location_field/static/js/map_input.js
+++ mapbox_location_field/static/js/map_input.js
@@ -1,3 +1,47 @@
+var coordinatesGeocoder = function (query) {
+// match anything which looks like a decimal degrees coordinate pair
+    var matches = query.match(/^[ ]*(?:Lat: )?(-?\d+\.?\d*)[, ]+(?:Lng: )?(-?\d+\.?\d*)[ ]*$/i);
+    if (!matches) {
+        return null;
+    }
+
+    function coordinateFeature(lng, lat) {
+        return {
+            center: [lng, lat],
+            geometry: {
+                type: "Point",
+                coordinates: [lng, lat]
+            },
+            place_name: 'Lat: ' + lat + ' Lng: ' + lng, // eslint-disable-line camelcase
+            place_type: ['coordinate'], // eslint-disable-line camelcase
+            properties: {},
+            type: 'Feature'
+        };
+    }
+
+    var coord1 = Number(matches[1]);
+    var coord2 = Number(matches[2]);
+    var geocodes = [];
+
+    if (coord1 < -90 || coord1 > 90) {
+// must be lng, lat
+        geocodes.push(coordinateFeature(coord1, coord2));
+    }
+
+    if (coord2 < -90 || coord2 > 90) {
+// must be lat, lng
+        geocodes.push(coordinateFeature(coord2, coord1));
+    }
+
+    if (geocodes.length === 0) {
+// else could be either lng, lat or lat, lng
+        geocodes.push(coordinateFeature(coord1, coord2));
+        geocodes.push(coordinateFeature(coord2, coord1));
+    }
+
+    return geocodes;
+};
+
 function translate_to_string(obj) {
     var lat = obj.lat;
     var lng = obj.lng;
@@ -11,9 +55,12 @@ var map = new mapboxgl.Map({
     center: [17.031645, 51.106715],
     zoom: 13
 });
+
 var geocoder = new MapboxGeocoder({
     accessToken: mapboxgl.accessToken,
     mapboxgl: mapboxgl,
+    localGeocoder: coordinatesGeocoder,
+
 });
 
 map.getCanvas().style.cursor = 'pointer';
@@ -24,7 +71,7 @@ map.addControl(new mapboxgl.GeolocateControl({
     positionOptions: {
         enableHighAccuracy: true
     },
-    trackUserLocation: true
+    trackUserLocation: true,
 }));
 map.addControl(geocoder, "top-left");
 map.addControl(new mapboxgl.FullscreenControl());
