fbf6bf98e27bdfb01a2258dfaf8a12287b68bc2a
simon <simon_rekin@o2.pl>
2019-06-30 22:49:59 +0200

    fixed case when map_attrs is None

--- mapbox_location_field/widgets.py
+++ mapbox_location_field/widgets.py
@@ -45,7 +45,8 @@ class MapInput(TextInput):
             "navigation_buttons": True,
             "track_location_button": True,
         }
-        default_map_attrs.update(self.map_attrs)
+        if self.map_attrs is not None:
+            default_map_attrs.update(self.map_attrs)
         xd = "<script>mapboxgl.accessToken = '{}';{}</script>".format(settings.MAPBOX_KEY,
                                                                       self.map_attrs_to_javascript(default_map_attrs))
         return xd
