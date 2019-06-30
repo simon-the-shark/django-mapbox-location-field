cd7349b0e018a2cfa069f86da65e8561708b9c9c
simon <simon_rekin@o2.pl>
2019-06-30 23:13:40 +0200

    deleted unnessesary attr

--- mapbox_location_field/widgets.py
+++ mapbox_location_field/widgets.py
@@ -17,7 +17,6 @@ class MapInput(TextInput):
 
     def get_context(self, name, value, attrs):
         must_be_attrs = {
-            "maxlenght": 63,
             "readonly": True,
             "placeholder": "Pick a location on map below"
         }
