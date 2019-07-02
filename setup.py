6f7c094562257f76e7decfec9da9cdf590de4178
simon <simon_rekin@o2.pl>
2019-07-02 12:06:28 +0200

    finally proper setup to pypi

--- setup.py
+++ setup.py
@@ -9,14 +9,14 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-mapbox-location-field',
-    version='0.1.3',
-    packages=find_packages(),
+    version='0.1.5',
+    packages=["mapbox_location_field"],
     include_package_data=True,
     license='MIT License',
     description='location field with MapInput widget for picking some location',
     long_description=README,
     long_description_content_type="text/markdown",
-    download_url="https://github.com/Simon-the-Shark/django-mapbox-location-field/archive/v0.1.3.tar.gz",
+    download_url="https://github.com/Simon-the-Shark/django-mapbox-location-field/archive/v0.1.5.tar.gz",
     url='https://github.com/Simon-the-Shark/django-mapbox-location-field',
     author='Szymon Kowaliński',
     author_email='simonrekin@gmail.com',
