a1419ffccab0db24d9adee604f01267d22ecce07
simon <simon_rekin@o2.pl>
2019-07-03 16:55:18 +0200

    added testapp for Travis CI purposes

--- manage.py
+++ manage.py
@@ -5,7 +5,7 @@ import sys
 
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'travis_settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'testapp.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
