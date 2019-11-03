3cc1847931bb9d048b766696c2d75f20f2187340
simon <simon_rekin@o2.pl>
2019-11-03 15:27:24 +0100

    added first tests

--- mapbox_location_field/tests/test_forms.py
+++ mapbox_location_field/tests/test_forms.py
@@ -1,6 +1,7 @@
+from django.contrib.gis.geos import Point
 from django.test import TestCase
 
-from mapbox_location_field.forms import LocationField, AddressAutoHiddenField
+from mapbox_location_field.forms import LocationField, AddressAutoHiddenField, SpatialLocationField
 from mapbox_location_field.widgets import MapInput, AddressAutoHiddenInput
 
 
@@ -17,6 +18,25 @@ class LocationFieldTests(TestCase):
     def test_passing_map_attrs(self):
         field = LocationField(map_attrs={"some": "value", "and some": "cool value"})
         self.assertEqual(field.widget.map_attrs, {"some": "value", "and some": "cool value"})
+
+
+class SpatialLocationFieldTests(TestCase):
+
+    def test_widget(self):
+        field = SpatialLocationField()
+        self.assertEqual(field.widget.__class__, MapInput().__class__)
+
+    def test_error_messages(self):
+        field = SpatialLocationField()
+        self.assertEqual(field.error_messages["required"], "Please pick a location, it's required")
+
+    def test_passing_map_attrs(self):
+        field = SpatialLocationField(map_attrs={"some": "value", "and some": "cool value"})
+        self.assertEqual(field.widget.map_attrs, {"some": "value", "and some": "cool value"})
+
+    def test_clean(self):
+        field = SpatialLocationField()
+        self.assertIsInstance(field.clean("12,12"), Point)
 
 
 class AddressAutoHiddenFieldTests(TestCase):
