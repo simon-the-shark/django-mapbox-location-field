e878ebc55c643cf0776f6a65d76434002d18d342
edgar <edgarfur@gmail.com>
2020-07-01 20:59:07 -0500

    Multiple location fields on the same page.

--- mapbox_location_field/static/mapbox_location_field/js/map_input.js
+++ mapbox_location_field/static/mapbox_location_field/js/map_input.js
@@ -62,76 +62,80 @@ if (!mapboxgl.supported()) {
             return [array[1], array[0]]
         }
 
-        var input = $(".js-mapbox-input-location-field");
-        var map = new mapboxgl.Map({
-            container: 'secret-id-map-mapbox-location-field',
-            style: map_attr_style,
-            center: map_attr_center,
-            zoom: map_attr_zoom,
-        });
-        if (input.val()) {
-            var marker = new mapboxgl.Marker({draggable: false, color: map_attr_marker_color,});
-            marker.setLngLat(map_attr_center)
-                .addTo(map);
-            input.val(replace_order(map_attr_center));
-        }
+        $(".js-mapbox-input-location-field").each(function (index) {
+            var input = $(this);
+            var id = input.attr("id");
+            var map = new mapboxgl.Map({
+                container: id + '-map-mapbox-location-field',
+                style: map_attr_style,
+                center: map_attr_center,
+                zoom: map_attr_zoom,
+            });
+            if (input.val()) {
+                var marker = new mapboxgl.Marker({draggable: false, color: map_attr_marker_color,});
+                marker.setLngLat(map_attr_center)
+                    .addTo(map);
+                input.val(replace_order(map_attr_center));
+            }
 
-        var geocoder = new MapboxGeocoder({
-            accessToken: mapboxgl.accessToken,
-            mapboxgl: mapboxgl,
-            localGeocoder: coordinatesGeocoder,
+            var geocoder = new MapboxGeocoder({
+                accessToken: mapboxgl.accessToken,
+                mapboxgl: mapboxgl,
+                localGeocoder: coordinatesGeocoder,
 
-        });
-
-        map.getCanvas().style.cursor = map_attr_cursor_style;
-        if (!map_attr_rotate) {
-            map.dragRotate.disable();
-            map.touchZoomRotate.disableRotation();
-        }
-        if (map_attr_track_location_button) {
-            map.addControl(new mapboxgl.GeolocateControl({
-                positionOptions: {
-                    enableHighAccuracy: true
-                },
-                trackUserLocation: true,
-            }));
-        }
-        if (map_attr_geocoder) {
-            map.addControl(geocoder, "top-left");
-        }
-
-        if (map_attr_fullscreen_button) {
-            map.addControl(new mapboxgl.FullscreenControl());
-        }
-        if (map_attr_navigation_buttons) {
-            map.addControl(new mapboxgl.NavigationControl());
-        }
-        geocoder.on("result", function (e) {
-            $("div.mapboxgl-marker.mapboxgl-marker-anchor-center").not(".mapboxgl-user-location-dot").remove();
-            input.val(replace_order(e.result.geometry.coordinates));
-            var marker = new mapboxgl.Marker({draggable: false, color: map_attr_marker_color,});
-            marker.setLngLat(e.result.geometry.coordinates)
-                .addTo(map);
-
-            $(document).trigger("reverse-geocode", [e.result.place_name,])
-        });
-
-        map.on("click", function (e) {
-            $("div.mapboxgl-marker.mapboxgl-marker-anchor-center").not(".mapboxgl-user-location-dot").remove();
-            input.val(translate_to_reversed_string(e.lngLat));
-            var marker = new mapboxgl.Marker({draggable: false, color: map_attr_marker_color,});
-            marker.setLngLat(e.lngLat)
-                .addTo(map);
-
-
-            var url = "https://api.mapbox.com/geocoding/v5/mapbox.places/" + translate_to_string(e.lngLat) + ".json?access_token=" + mapboxgl.accessToken;
-            $.get(url, function (data) {
-                reverse_name = data.features[0].place_name;
-                geocoder.setInput(reverse_name);
-                $(document).trigger("reverse-geocode", [reverse_name,]);
             });
 
+            map.getCanvas().style.cursor = map_attr_cursor_style;
+            if (!map_attr_rotate) {
+                map.dragRotate.disable();
+                map.touchZoomRotate.disableRotation();
+            }
+            if (map_attr_track_location_button) {
+                map.addControl(new mapboxgl.GeolocateControl({
+                    positionOptions: {
+                        enableHighAccuracy: true
+                    },
+                    trackUserLocation: true,
+                }));
+            }
+            if (map_attr_geocoder) {
+                map.addControl(geocoder, "top-left");
+            }
+
+            if (map_attr_fullscreen_button) {
+                map.addControl(new mapboxgl.FullscreenControl());
+            }
+            if (map_attr_navigation_buttons) {
+                map.addControl(new mapboxgl.NavigationControl());
+            }
+            geocoder.on("result", function (e) {
+                $("div.mapboxgl-marker.mapboxgl-marker-anchor-center").not(".mapboxgl-user-location-dot").remove();
+                input.val(replace_order(e.result.geometry.coordinates));
+                var marker = new mapboxgl.Marker({draggable: false, color: map_attr_marker_color,});
+                marker.setLngLat(e.result.geometry.coordinates)
+                    .addTo(map);
+
+                $(document).trigger("reverse-geocode", [e.result.place_name,])
+            });
+
+            map.on("click", function (e) {
+                $("#" + id + "-map-mapbox-location-field .mapboxgl-marker.mapboxgl-marker-anchor-center").not(".mapboxgl-user-location-dot").remove();
+                input.val(translate_to_reversed_string(e.lngLat));
+                var marker = new mapboxgl.Marker({draggable: false, color: map_attr_marker_color,});
+                marker.setLngLat(e.lngLat)
+                    .addTo(map);
+
+
+                var url = "https://api.mapbox.com/geocoding/v5/mapbox.places/" + translate_to_string(e.lngLat) + ".json?access_token=" + mapboxgl.accessToken;
+                $.get(url, function (data) {
+                    reverse_name = data.features[0].place_name;
+                    geocoder.setInput(reverse_name);
+                    $(document).trigger("reverse-geocode", [reverse_name,]);
+                });
+
+            });
         });
+
 
         var addressinput = $(".js-mapbox-address-input-location-field");
         if (addressinput.val()) {
