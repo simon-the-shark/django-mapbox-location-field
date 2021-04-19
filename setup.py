767ef1a69b7660768cc576c28837fb498757756c
Szymon Kowaliński <szymon@kowalinski.dev>
2021-04-19 23:46:08 +0200

    setup.py upload

--- setup.py
+++ setup.py
@@ -10,14 +10,14 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-mapbox-location-field',
-    version='1.7.2',
+    version='1.7.3',
     packages=["mapbox_location_field"],
     include_package_data=True,
     license='MIT License',
     description='location field with MapInput widget for picking some location',
     long_description=README,
     long_description_content_type="text/markdown",
-    download_url="https://github.com/Simon-the-Shark/django-mapbox-location-field/archive/v1.7.2.tar.gz",
+    download_url="https://github.com/Simon-the-Shark/django-mapbox-location-field/archive/v1.7.3.tar.gz",
     url='https://github.com/Simon-the-Shark/django-mapbox-location-field',
     author='Szymon Kowaliński',
     author_email='contact@kowalinski.dev',
