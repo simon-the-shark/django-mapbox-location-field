123bb512dfe6204ffca66fd7e791cfc8225f4c84
h3x4git <57725095+h3x4git@users.noreply.github.com>
2022-02-11 17:53:28 +0100

    This fixes #32
    this fixes #32

--- mapbox_location_field/static/mapbox_location_field/js/address_input.js
+++ mapbox_location_field/static/mapbox_location_field/js/address_input.js
@@ -1,3 +1,6 @@
-$(document).on("reverse-geocode", function (e, map_id, input) {
-    $("#" + map_id + ".js-mapbox-address-input-location-field").val(input);
+$(function() {
+  $(document).on("reverse-geocode", function (e, map_id, input) {
+    x = $("#" + map_id + ".js-mapbox-address-input-location-field");
+    x.val(input);
+  });
 });
