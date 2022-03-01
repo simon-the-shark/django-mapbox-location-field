5bbff127a5189e56f91db999510b799de45c868e
Szymon Kowaliński <szymon@kowalinski.dev>
2022-03-01 21:17:16 +0100

    trying to set up gh actions

--- testapp/settings.py
+++ testapp/settings.py
@@ -69,9 +69,9 @@ TEMPLATES = [
 ]
 
 #
-GDAL_LIBRARY_PATH = '/opt/homebrew/opt/gdal/lib/libgdal.dylib'
-GEOS_LIBRARY_PATH = '/opt/homebrew/opt/geos/lib/libgeos_c.dylib'
-#
+# GDAL_LIBRARY_PATH = '/opt/homebrew/opt/gdal/lib/libgdal.dylib'
+# GEOS_LIBRARY_PATH = '/opt/homebrew/opt/geos/lib/libgeos_c.dylib'
+# #
 
 WSGI_APPLICATION = 'testapp.wsgi.application'
 
